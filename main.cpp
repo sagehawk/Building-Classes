16f8d500cb1061f93b0b5f64aa20462f652fcd98
Sajjad <haq.sajjad220@gmail.com>
2023-03-09 09:04:34 +0000

    Began working on class Complex. Created doubles and constructors.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,16 @@
 #include <cmath>
 #include <iostream>
 
+using namespace std;
+
+class Complex {
+private:
+  double real;
+  double imaginary;
+
+public:
+  Complex() : real(0), imaginary(0) {}
+  Complex(double r, double i) : real(r), imaginary(i) {}
+};
+
 int main() {}
