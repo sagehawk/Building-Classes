02fd2dc79c783d689b63570a7ae868c66af20615
Sajjad <haq.sajjad220@gmail.com>
2023-03-14 04:58:47 +0000

    Finished

--- .ccls-cache/@home@runner@3-Building-Classes-1-SageHaq/main.cpp
+++ .ccls-cache/@home@runner@3-Building-Classes-1-SageHaq/main.cpp
@@ -27,18 +27,38 @@ public:
   Complex operator+(const Complex &other) const {
     return Complex(real + other.real, imag + other.imag);
   }
+
   // Subtraction
+  Complex operator-(const Complex &other) const {
+    return Complex(real - other.real, imag - other.imag);
+  }
 
   // Multiplication
+  Complex operator*(const Complex &other) const {
+    double realPart = real * other.real - imag * other.imag;
+    double imagPart = real * other.imag + imag * other.real;
+    return Complex(realPart, imagPart);
+  }
 
   // division
-
+  Complex operator/(const Complex &other) const {
+    double denom = other.real * other.real + other.imag * other.imag;
+    double realPart = (real * other.real + imag * other.imag) / denom;
+    double imagPart = (imag * other.real - real * other.imag) / denom;
+    return Complex(realPart, imagPart);
+  }
   // negation
-
+  Complex operator-() const {
+    return Complex(-real, -imag);
+  }
   // magnitude
-
+  double magnitude() const {
+    return sqrt(real * real + imag * imag);
+  }
   // conjugate
-  // Print out Complex numbers
+  Complex conjugate() const {
+    return Complex(real, -imag);
+  }
 };
 
 ostream &operator<<(ostream &os, const Complex &num) {
@@ -56,7 +76,7 @@ istream &operator>>(istream &is, Complex &num) {
 
 int main() {
   // Example Complex numbers
-  Complex a(3, 5);
+  Complex a(3, -5);
   Complex b(6, 8);
 
   // Example for every operation
@@ -64,8 +84,17 @@ int main() {
   cout << "b = " << b << endl;
 
   cout << "a + b = " << a + b << endl;
+  cout << "a - b = " << a - b << endl;
+  cout << "a * b = " << a * b << endl;
+  cout << "a / b = " << a / b << endl;
 
-  // cout << "|a| = " << a.magnitude() << endl;
+  cout << "|a| = " << a.magnitude() << endl;
+  cout << "Conjugate of a = " << a.conjugate() << endl;
+
+  Complex c;
+  cout << "Enter a complex number: ";
+  cin  >> c;
+  cout << "You entered " << c << endl;
 
   return 0;
 }
